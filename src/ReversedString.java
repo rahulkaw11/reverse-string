8b846dc0d35f5482616b0306dac346eb87662253
rahulkaw11 <rahulkawatghare05@gmail.com>
2024-01-28 18:47:58 +0530

    2nd commit

--- src/ReversedString.java
+++ src/ReversedString.java
@@ -5,6 +5,7 @@ public class ReversedString {
 	public static void main (String[] args) {
 		
 		String myName = "Rahul Kawatghare";
+		String name = "Rahul Kawatghare";
 		String reverseMyName = reverseString(myName);
 		
 		System.out.print(reverseMyName);
